82c59d3bddd7de8b24e47c557b1c6a435054d728
Abdelaziz El idrissi <57861799+roudlek@users.noreply.github.com>
2024-01-01 00:10:04 +0100

    Update next.config.js second time

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    output: 'export',
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'tailwindui.com', 
-          port: '',
-          pathname: '/img/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'fakestoreapi.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tailwindui.com', 
+        port: '',
+        pathname: '/img/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  output: 'export', // Add this line to enable static export
+};
