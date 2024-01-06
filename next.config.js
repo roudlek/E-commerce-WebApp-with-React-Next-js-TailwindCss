652b73477e23b04963aa147fc31f83b886479fe4
abdelaziz el idrissi <elidrissiabdalaziz@gmail.com>
2024-01-06 01:05:01 +0100

    all good

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  // output: "export", // Add this line to enable static export
+  output: "export", // Add this line to enable static export
   images: {
     remotePatterns: [
       {
